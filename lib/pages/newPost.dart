810e7dad554ecddc90518230ab88d1ece69c2491
Lord Atakora <Inquire@obsidianrune.com>
2023-08-10 18:58:57 -0400

    updates to newpost page

--- lib/pages/newPost.dart
+++ lib/pages/newPost.dart
@@ -1,10 +1,15 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart'; //This is the push and pull
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:journey2/auth.dart';
 import 'package:journey2/constants.dart';
+import 'package:journey2/widget_tree.dart';
 import 'package:journey2/pages/ProfileView.dart';
 import 'package:journey2/pages/home_page.dart';
 
@@ -19,6 +24,11 @@ class _NewPostState extends State<NewPost> {
 //Global Variables/functions
   final TextEditingController _newPostTextController = TextEditingController();
   var errorMsg = "";
+  File? _image;
+  var selectedImg = "";
+  File? selectedGalleryImg;
+  bool selectedFile = false;
+
   //Push Data to Firebase
 
   //This is the area to get the text from the user
@@ -37,6 +47,130 @@ class _NewPostState extends State<NewPost> {
             enabledBorder: const UnderlineInputBorder(
               borderSide: BorderSide(color: Colors.white),
             )));
+  }
+
+  Future _getImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (image == null) return;
+
+    final imageTemp = File(image.path);
+
+    print("Image collected : " + imageTemp.toString());
+
+    setState(() {
+      _image = imageTemp;
+      selectedGalleryImg = _image;
+      selectedFile = true;
+
+      // //Test
+      // print("\n \n \n ");
+      // print("_Image Value: " + _image.toString() + "\n");
+      // print(
+      //     "selectedGalleryImg Value: " + selectedGalleryImg.toString() + "\n");
+      // print("selectedFile Value: " + selectedFile.toString() + "\n");
+      // print("\n \n \n ");
+    });
+  }
+
+  Future selectFile() async {
+    final selectedImgs = [];
+    final selection =
+        await FilePicker.platform.pickFiles(allowCompression: true);
+    if (selection != null) {
+      for (var element in selection.files) {
+        selectedImgs.add(element.path);
+      }
+    }
+  }
+
+  //This is the area to get the text from the user
+  Widget _entryPostField(
+      String type, String title, TextEditingController controller, Size size) {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    File stockFile;
+    return Container(
+      height: size.height * 0.5,
+      width: size.width * 0.85,
+      color: Color.fromARGB(0, 158, 158, 158),
+      child: Column(
+        children: [
+          Padding(
+              padding: EdgeInsets.all(3),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    FirebaseAuth.instance.currentUser?.photoURL as String),
+                radius: size.width * 0.125,
+              )),
+          //Area for Image if not null
+          if (selectedGalleryImg != null && selectedFile) ...[
+            Card(
+              child: Image.asset(
+                fit: BoxFit.cover,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              elevation: 5,
+              margin: EdgeInsets.all(10),
+            )
+          ],
+          TextField(
+              keyboardType: TextInputType.multiline,
+              minLines: 1, //Normal textInputField will be displayed
+              maxLines: 7,
+              style: const TextStyle(color: Colors.white),
+              controller: controller,
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color.fromARGB(0, 37, 32, 32),
+                  hintText: title,
+                  hintStyle: const TextStyle(color: Colors.white),
+                  enabledBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ))),
+          Row(
+            children: [
+              IconButton(
+                  color: Colors.white,
+                  onPressed: _getImage,
+                  icon: Icon(Icons.add_a_photo_rounded)),
+              GestureDetector(
+                onTap: _getImage,
+                child: Text(
+                  "Add A photo",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: size.height * 0.03,
+          ),
+          GestureDetector(
+            onTap: () {
+              _postToFirebase();
+            },
+            child: Card(
+              color: kPrimaryAccentColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                Radius.circular(25.0),
+              )),
+              elevation: 4.0,
+              child: Container(
+                  width: size.width * 0.6,
+                  height: size.height * 0.06,
+                  child: Center(
+                    child: Text("SAVE",
+                        style: TextStyle(
+                            color: Colors.white, fontSize: size.width * 0.05)),
+                  )),
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   Widget _saveButton() {
@@ -66,10 +200,14 @@ class _NewPostState extends State<NewPost> {
     var formatDate = new DateFormat('mm/dd/yyyy - kk:mm');
     String cleanDate = formatDate.format(now);
     await FirebaseFirestore.instance.collection("Posts").add({
-      "userKey": currentUser?.uid,
+      "Date": DateFormat.yMd(),
+      "Img": "",
+      "LikesNum": "0",
+      "TimeStamp": "Thu JUN 8 7:08PM",
       "text": _newPostTextController.text,
+      "type": "Standard",
+      "userKey": currentUser?.uid,
       'date': cleanDate.toString(),
-      "TimeStamp": "Thu JUN 8 7:08PM"
     }).whenComplete(() => {
           Navigator.push(
             context,
@@ -115,7 +253,7 @@ class _NewPostState extends State<NewPost> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
-              height: size.height * 0.1,
+              height: size.height * 0.05,
             ),
             Card(
               elevation: 4.0,
@@ -128,7 +266,7 @@ class _NewPostState extends State<NewPost> {
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       SizedBox(
-                        height: size.height * 0.05,
+                        height: size.height * 0.005,
                       ),
                       Text(
                         errorMsg,
@@ -137,7 +275,7 @@ class _NewPostState extends State<NewPost> {
                             fontWeight: FontWeight.bold),
                       ),
                       Text(
-                        "New Post to Ride Along",
+                        "New Post",
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: size.width * 0.07,
@@ -145,30 +283,13 @@ class _NewPostState extends State<NewPost> {
                         ),
                       ),
                       SizedBox(
-                        height: size.height * 0.05,
+                        height: size.height * 0.005,
                       ),
-                      _entryField("Enter Text. . . ", _newPostTextController),
+                      _entryPostField("Standard", "Enter Text. . . ",
+                          _newPostTextController, size),
                       SizedBox(
                         height: size.height * 0.05,
                       ),
-                      GestureDetector(
-                        onTap: () {
-                          _postToFirebase();
-                        },
-                        child: Card(
-                          elevation: 4.0,
-                          child: Container(
-                              color: kPrimaryAccentColor,
-                              width: size.width * 0.9,
-                              height: size.height * 0.08,
-                              child: Center(
-                                child: Text("SAVE",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: size.width * 0.08)),
-                              )),
-                        ),
-                      )
 
                       // _saveButton()
                     ],
